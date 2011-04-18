1a4b4d17f97643c5697732b033713fa818c6d824
Puskin <mbpuskin@gmail.com>
2011-04-18 22:36:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jetu::Application.config.secret_token = '1351ac2eb4bc6992d0f0c3dacc9de83d0ced3844ff89bc0ea3dc9e0df070c83e59fa18c4a9440d2c5969faca6dab0243c4c66adf52c169d6fe6b0995dfd11545'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jetu::Application.config.session_store :cookie_store, :key => '_jetu_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jetu::Application.config.session_store :active_record_store
