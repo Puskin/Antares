6c9c520355688b01d8de279d8976b97c952675da
Puskin <mbpuskin@gmail.com>
2011-06-21 17:55:54 +0200

    Domain fixing add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery
   
-  before_filter :set_timezone                        
-  
+  before_filter :set_timezone             
+  before_filter :check_uri
+  	 	
+    def check_uri	
+      redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
+    end           
 
     def set_timezone
       Time.zone = "Warsaw"
